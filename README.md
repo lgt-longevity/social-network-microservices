b04431b5a47d4190e7230b01bbeee674d4a68cde
Fábio Maia <fabio.maia@itsector.pt>
2022-02-04 19:28:57 +0000

    feat: initial sources

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to LGT - Longevity Social Network Micro Services
+
+This is a source code of LGT - Longevity Social Network Micro Services
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('LGT - Longevity Social Network API')
+    .setDescription('The LGT - Longevity Social Network API')
+    .setVersion('0.1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(5000);
+}
+bootstrap();
